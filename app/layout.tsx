57bd37aa52fd0a391ba3af2c63707e2594106b70
Rajan <rajanj448@student.sfit.ac.in>
2023-12-27 19:05:00 +0530

    Dark & Light Theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -20,18 +21,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-      <body className={font.className}>
+      <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338]"
+        )}>
         <ThemeProvider
          attribute="class"
          defaultTheme="dark"
          enableSystem={false}
          storageKey="discord-theme"
         >
+           {children}
 
 
         </ThemeProvider>
         
-        {children}</body>
+       </body>
     </html>
     </ClerkProvider>
   )
